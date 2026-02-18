bdeeb950eb2ea941d7c88e3492582ad9b63f2b24
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2026-02-18 07:47:55 -0500

    Resolved #3.

---
+++ terraform/setup-confluent-environment.tf
@@ -0,0 +1,24 @@
+resource "confluent_environment" "non_prod" {
+  display_name = "non-prod"
+
+  stream_governance {
+    package = "ESSENTIALS"
+  }
+}
+
+resource "confluent_gateway" "non_prod" {
+  display_name = "${confluent_environment.non_prod.display_name}-pni-gateway"
+
+  environment {
+    id = confluent_environment.non_prod.id
+  }
+
+  aws_private_network_interface_gateway {
+    region = var.aws_region
+    zones  = local.available_zones
+  }
+
+  depends_on = [ 
+    confluent_environment.non_prod 
+  ]
+}
