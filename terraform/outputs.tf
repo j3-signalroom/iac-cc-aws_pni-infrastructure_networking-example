c3af4b330976f285f4fc56517cc4d1615439677f
Jeffrey Jonathan Jennings (J3) <j3@signalroom.io>
2026-02-19 18:32:55 -0500

    Resolved #27.

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -7,3 +7,18 @@ output "confluent_pni_hub_access_point_id" {
   description = "Confluent PNI hub access point ID"
   value       = module.pni_hub.confluent_pni_hub_access_point_id
 }
+
+output "confluent_environment_id" {
+  description = "Confluent Cloud Environment ID"
+  value       = confluent_environment.non_prod.id
+} 
+
+output "confluent_sandbox_kafka_cluster_id" {
+  description = "Confluent Cloud Sandbox Kafka Cluster ID"
+  value       = confluent_kafka_cluster.sandbox_cluster.id
+}
+
+output "confluent_shared_kafka_cluster_id" {
+  description = "Confluent Cloud Shared Kafka Cluster ID"
+  value       = confluent_kafka_cluster.shared_cluster.id
+}
